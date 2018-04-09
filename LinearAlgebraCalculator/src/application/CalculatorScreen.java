99e5b13a18ba2fbbe00c35b5581fd582e207fbbe
priyaVora <prvora89@gmail.com>
2018-04-08 22:23:15 -0600

    Have the Matrix Calculator Jar Added to the build path. Have the basic controller and fxml file connection!

--- LinearAlgebraCalculator/src/application/CalculatorScreen.java
+++ LinearAlgebraCalculator/src/application/CalculatorScreen.java
@@ -1,5 +1,34 @@
 package application;
 
-public class CalculatorScreen {
+import java.net.URL;
+import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class CalculatorScreen {
+	@FXML
+	private Button calculateButton = new Button();
+	@FXML
+	private TextField firstRowSizeField = new TextField();
+	@FXML
+	private TextField secondRowSizeField = new TextField();
+	@FXML
+	private TextField firstColSizeField = new TextField();
+	@FXML
+	private TextField secondColSizeField = new TextField();
+
+	@FXML
+	private void calculateButtonAction(ActionEvent event) {
+		String firstRowSize = firstRowSizeField.getText().trim();
+		String firstColSize = firstColSizeField.getText().trim();
+		String secondRowSize = secondRowSizeField.getText().trim();
+		String secondColSize = secondColSizeField.getText().trim();
+		
+	}
 }
--- LinearAlgebraCalculator/src/application/Main.java
+++ LinearAlgebraCalculator/src/application/Main.java
@@ -1,26 +1,30 @@
 package application;
-	
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.stage.Stage;
+import model.Matrix;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 
-
 public class Main extends Application {
+
 	@Override
-	public void start(Stage primaryStage) {
-		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+	public void start(Stage primaryStage) throws Exception {
+		Parent root = FXMLLoader.load(getClass().getResource("/CalculatorScreen.fxml"));
+
+		Scene scene = new Scene(root);
+		scene.getStylesheets().add(Main.class.getResource("application.css").toExternalForm());
+
+		primaryStage.setScene(scene);
+		primaryStage.setTitle("Matrix Calculator");
+
+		primaryStage.show();
 	}
-	
+
 	public static void main(String[] args) {
 		launch(args);
+
 	}
 }
