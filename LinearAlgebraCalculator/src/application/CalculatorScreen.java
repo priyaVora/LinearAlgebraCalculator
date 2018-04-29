d36ccc3b07f8c2b612492770e76ea80899280919
priyaVora <prvora89@gmail.com>
2018-04-29 00:27:56 -0600

    Determinant Function can show work now!

--- LinearAlgebraCalculator/src/application/CalculatorScreen.java
+++ LinearAlgebraCalculator/src/application/CalculatorScreen.java
@@ -965,6 +965,15 @@ public class CalculatorScreen implements Initializable {
 							// TODO Auto-generated catch block
 							e1.printStackTrace();
 						}
+					} else if (operationChoiceBox.getItems()
+							.get(operationChoiceBox.getSelectionModel().getSelectedIndex()).equals("Determinant")) {
+						try {
+							Desktop.getDesktop().open(new java.io.File(System.getProperty("user.home") + "/Desktop"
+									+ "\\MatrixShowWork" + "\\Determinant.txt"));
+						} catch (IOException e1) {
+							// TODO Auto-generated catch block
+							e1.printStackTrace();
+						}
 					}
 
 				}
